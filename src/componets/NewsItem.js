31a7f679091a78e2caf0f1a07c7d88cea8dca30a
Muhammmadhashim-rgb <muhammadhashim4412@gmail.com>
2025-07-21 22:59:40 +0500

    API Fecht

--- src/componets/NewsItem.js
+++ src/componets/NewsItem.js
@@ -6,11 +6,11 @@ export default class NewsItem extends Component {
         return (
             <div>
                 <div className="card my-3" style={{ width: "18rem" }}>
-                    <img src={imageurl} className="card-img-top" alt="/" />
+                    <img src={!imageurl?"https://techcrunch.com/wp-content/uploads/2025/07/Screenshot-2025-07-14-at-2.44.29PM.png?resize=1200,616":imageurl} className="card-img-top" alt="/" />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
-                        <a href={newsurl} target='_blank' className="btn btn-sm btn-primary">Read More</a>
+                        <a href={newsurl} target='_blank' className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
